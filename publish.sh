6850616acad1169ac20913572709df11133c0322
Loric Brevet <loric.brevet@gmail.com>
2024-09-20 17:59:25 +0200

    Create publish script

---
+++ publish.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+version=$1
+
+if [ -z "$version" ]; then
+    echo "missing version as argument"
+    exit 1
+fi
+
+bin="terraform-provider-commercelayer_v${version}"
+archive="terraform-provider-commercelayer_${version}_linux_amd64.zip"
+sha="terraform-provider-commercelayer_${version}_SHA256SUMS"
+manifest="terraform-provider-commercelayer_${version}_manifest.json"
+sig="terraform-provider-commercelayer_${version}_SHA256SUMS.sig"
+
+dist="dist/$version"
+
+
+echo "remove old dist $dist"
+rm -rf "$dist"
+
+echo "create $dist"
+mkdir "$dist"
+
+echo "build go code"
+CGO_ENABLED=0 go build -o "$bin"
+
+echo "create archive $archive"
+zip "$archive" "$bin"
+
+echo "move archive to dist"
+mv "$archive" "$dist/"
+
+echo "remove binary"
+rm "$bin"
+
+echo "create manifest"
+cp terraform-registry-manifest.json "$dist/$manifest"
+
+echo "create shasum"
+shasum -a 256 "$dist/$archive" > "$dist/$sha"
+shasum -a 256 "$dist/$manifest" >> "$dist/$sha"
+
+echo "add signature"
+gpg --output "$dist/$sig" --detach-sign "$dist/$sha"
